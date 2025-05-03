41fd31eee79b2bae6219d2751e24ee7c1ae4335f
Guy Oz <109523947+GuyOz5252@users.noreply.github.com>
2025-05-03 23:27:05 +0300

    Added camera follow player system

--- src/main.rs
+++ src/main.rs
@@ -20,12 +20,13 @@ fn main() {
         .run();
 }
 
-fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
-    let window = window_query.single();
-
+fn spawn_camera(mut commands: Commands) {
     commands.spawn((
         Camera2d {},
-        Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+        Transform {
+            translation: Vec3::ZERO,
+            ..Default::default()
+        },
     ));
 }
 
--- src/player.rs
+++ src/player.rs
@@ -17,8 +17,10 @@ pub struct PlayerPlugin;
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, spawn_player)
+            .add_systems(Update, camera_follow_player_system)
             .add_systems(Update, player_input_system)
-            .add_systems(Update, player_sprint_system);
+            .add_systems(Update, player_sprint_system)
+            .add_systems(Update, player_look_system);
     }
 }
 
@@ -51,6 +53,23 @@ fn spawn_player(
     ));
 }
 
+fn camera_follow_player_system(
+    mut camera_query: Query<&mut Transform, (With<Camera2d>, Without<Player>)>,
+    player_query: Query<&Transform, (With<Player>, Without<Camera2d>)>,
+    time: Res<Time>,
+) {
+    let mut camera_transform = camera_query.single_mut();
+    let player_transform = player_query.single();
+    
+
+    let camera_speed = 3.0;
+
+    camera_transform.translation = camera_transform.translation.lerp(
+        player_transform.translation,
+        camera_speed * time.delta_secs(),
+    );
+}
+
 fn player_input_system(
     keyboard: Res<ButtonInput<KeyCode>>,
     mut query: Query<(&mut Velocity, &Movement), With<Player>>,
@@ -80,11 +99,29 @@ fn player_sprint_system(
     keyboard: Res<ButtonInput<KeyCode>>,
     mut query: Query<&mut Movement, With<Player>>,
 ) {
-    if let Ok(mut movement) = query.get_single_mut() {
-        if keyboard.pressed(KeyCode::ShiftLeft) {
-            movement.speed_multiplier = PLAYER_SPRINT_MULTIPLIER;
-        } else {
-            movement.speed_multiplier = 1.0;
+    let mut movement = query.single_mut();
+
+    if keyboard.pressed(KeyCode::ShiftLeft) {
+        movement.speed_multiplier = PLAYER_SPRINT_MULTIPLIER;
+    } else {
+        movement.speed_multiplier = 1.0;
+    }
+}
+
+fn player_look_system(
+    window: Query<&Window, With<PrimaryWindow>>,
+    camera: Query<(&Camera, &GlobalTransform), With<Camera2d>>,
+    mut player: Query<&mut Transform, With<Player>>,
+) {
+    let window = window.single();
+    let (camera, camera_transform) = camera.single();
+    let mut player_transform = player.single_mut();
+
+    if let Some(curser_position) = window.cursor_position() {
+        if let Ok(world_position) = camera.viewport_to_world_2d(camera_transform, curser_position) {
+            let direction = world_position - player_transform.translation.truncate();
+            let angle = direction.y.atan2(direction.x);
+            player_transform.rotation = Quat::from_rotation_z(angle);
         }
     }
 }
